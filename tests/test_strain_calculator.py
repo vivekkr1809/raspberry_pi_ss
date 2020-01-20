c39a8af9ae47534cab1ceed192b51810e88bec51
Vivek Kumar <vivekk@princeton.edu>
2020-01-20 17:13:01 -0500

    test to calculate strain

---
+++ tests/test_strain_calculator.py
@@ -0,0 +1,12 @@
+import pytest
+
+from modules.strain_calculator import StrainCalculator
+
+def test_no_strain():
+	_eps = 1.0e-4
+	v_out_baseline = 1.
+	v_in_baseline = 1.
+	v_out_current = 1.+_eps
+	v_in_current = 1.-_eps
+	strain = StrainCalculator(v_out_baseline, v_in_baseline, v_out_current, v_in_current).compute_strain()
+	assert(strain==pytest.approx(0, abs=1.0e-5, rel=1.0e-5))
