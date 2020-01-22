e609d81fdd85d058bfaecb741bc982f80347f2fe
Vivek Kumar <vivekk@princeton.edu>
2020-01-22 14:02:34 -0500

    Created default values for gf and gain

--- source/modules/save_data_file.py
+++ source/modules/save_data_file.py
@@ -7,16 +7,22 @@ class SaveData():
   def __init__(self):
     pass
     
-  def save_baseline_vector(self, filename, pickle_file, gain, gauge_factor, out_mean_base, reference, reference_mean_base, sig_neg_mean_base, sig_pos_mean_base, time_bin_base, v_in_mean_base, v_out_mean_base):
+  def save_baseline_vector(self, filename, pickle_file, out_bin_base, reference, reference_mean_base, separated_base, sig_neg_mean_base, sig_pos_mean_base, time_bin_base, v_in_mean_base, v_out_mean_base, gain=None, gauge_factor=None):
 
-    baseline_dict = {'filename':None, 'gain':None, 'gauge_factor':None, 'out_mean_base': None, 'reference':None, 'reference_mean_base':None, 'sig_neg_mean_base':None, 'sig_pos_mean_base':None, 'time_bin_base':None, 'v_in_mean_base':None, 'v_out_mean_base':None}
+    # Set default values for gain and gauge factor
+    if gain is None:
+      gain = 200
+    if gauge_factor is None:
+      gauge_factor = 2.1
 
+    baseline_dict = {'filename':None, 'gain':None, 'gauge_factor':None, 'out_bin_base': None, 'reference':None, 'reference_mean_base':None, 'separated_base':None, 'sig_neg_mean_base':None, 'sig_pos_mean_base':None, 'time_bin_base':None, 'v_in_mean_base':None, 'v_out_mean_base':None}
     baseline_dict['filename'] = filename
     baseline_dict['gain'] = gain
     baseline_dict['gauge_factor'] = gauge_factor
-    baseline_dict['out_mean_base'] = out_mean_base
+    baseline_dict['out_bin_base'] = out_bin_base
     baseline_dict['reference'] = reference
     baseline_dict['reference_mean_base'] = reference_mean_base
+    baseline_dict['separated_base'] = separated_base
     baseline_dict['sig_neg_mean_base'] = sig_neg_mean_base
     baseline_dict['sig_pos_mean_base'] = sig_pos_mean_base
     baseline_dict['time_bin_base'] = time_bin_base
